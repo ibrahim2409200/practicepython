91f4361700f9a054e9cd4d2d27b8f4819f52a955
ibrahim2409200 <67977629+ibrahim2409200@users.noreply.github.com>
2021-03-17 19:23:02 +0500

    Update exercise4.py

--- exercise4.py
+++ exercise4.py
@@ -1,3 +1,4 @@
+#Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number
 n=int(input("Enter Range Number:"))
 dl=[]
 for i in range(1,n+1):
